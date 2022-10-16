346f56dc5270eba268f08b614c72fa08640a1d97
mahdi chekini <me.chekini@gmail.com>
2022-10-16 15:13:27 +0200

    Add features modules

---
+++ src/app/features/disponibilites/disponibilites.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DisponibilitesComponent } from './disponibilites.component';
+
+describe('DisponibilitesComponent', () => {
+  let component: DisponibilitesComponent;
+  let fixture: ComponentFixture<DisponibilitesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DisponibilitesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DisponibilitesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/disponibilites/disponibilites.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disponibilites',
+  templateUrl: './disponibilites.component.html',
+  styleUrls: ['./disponibilites.component.scss']
+})
+export class DisponibilitesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
