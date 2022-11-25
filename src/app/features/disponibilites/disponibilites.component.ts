b4f254af5d139954e41cf2c74386cabc6f517f64
Abderraouf SALAH <abderraouf.salah@intalio.com>
2022-11-25 15:12:33 +0100

    feature/disponibilites

--- src/app/features/disponibilites/disponibilites.component.ts
+++ src/app/features/disponibilites/disponibilites.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface TableData {
+  headerRow: string[];
+  dataRows: any[][];
+}
+
 @Component({
   selector: 'app-disponibilites',
   templateUrl: './disponibilites.component.html',
   styleUrls: ['./disponibilites.component.scss']
 })
 export class DisponibilitesComponent implements OnInit {
+  public tableData: TableData;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tableData = {
+        headerRow: [ 'Jour', 'Matin', 'Midi', 'Soir'],
+        dataRows: [
+            ['Lundi', false, false, false],
+            ['Mardi', false, false, false],
+            ['Mercredi', false, false, false],
+            ['Jeudi', false, false, false],
+            ['Vendredi', false, false, false],
+            ['Samedi', false, false, false],
+            ['Dimanche', false, false, false],
+        ]
+    };
   }
 
 }
