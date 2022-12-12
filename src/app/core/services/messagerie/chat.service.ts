61b1fd27d818284720d9720cabf6d7fea8b5d74c
mahdcheck <me.chekini@gmail.com>
2022-12-12 04:47:12 +0100

    update familleUrl Params

--- src/app/core/services/messagerie/chat.service.ts
+++ src/app/core/services/messagerie/chat.service.ts
@@ -22,7 +22,7 @@ export class ChatService {
 
   getListFamilles(): Observable<Famille[]> {
     return this.http.get<Famille[]>(
-      "api/v1/search/famille?nom=&prenom=&ville=&jour=&heureDebut=&heureFin="
+      "api/v1/search/famille?nom=&prenom=&ville=&jour=-1&heureDebut=&heureFin="
     );
   }
 
